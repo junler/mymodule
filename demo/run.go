bb29fc36b1cd66b69ce39bade45f6386126ef72c
junler <junler_top@qq.com>
2024-07-02 17:25:07 +0800

    second commit

---
+++ demo/run.go
@@ -0,0 +1,17 @@
+package demo
+
+import "fmt"
+
+func RunFor() {
+	ch := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
